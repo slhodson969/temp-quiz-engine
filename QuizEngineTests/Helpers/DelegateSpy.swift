77e789498a0150e801c2511c3a4439e52097b288
Scott Hodson <scott@keepitswift.com>
2023-12-05 22:08:34 +0100

    Code cleanup

--- QuizEngineTests/Helpers/DelegateSpy.swift
+++ QuizEngineTests/Helpers/DelegateSpy.swift
@@ -5,36 +5,6 @@
 import Foundation
 import QuizEngine
 
-struct ClosureHolder {
-    let closure: (() -> Void)?
-}
-
-//class DelegateSpy: QuizDelegate {
-//    var questionsAsked: [String] = []
-//    var goToPreviousQuestionCompletions: [ClosureHolder] = []
-//    var goToNextQuestionCompletions: [ClosureHolder] = []
-//    var answerCompletions: [(String) -> Void] = []
-//
-//    var completedQuizzes: [[(String, String)]] = []
-//        
-//    func answer(for question: String, goToPreviousQuestion: (() -> Void)?, goToNextQuestion: (() -> Void)?, completion: @escaping (String) -> Void) {
-//        questionsAsked.append(question)
-//        goToPreviousQuestionCompletions.append(ClosureHolder(closure: goToPreviousQuestion))
-//        goToNextQuestionCompletions.append(ClosureHolder(closure: goToNextQuestion))
-//        answerCompletions.append(completion)
-//    }
-//    
-//    func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
-//        completedQuizzes.append(answers)
-//    }
-//    
-//    deinit {
-//        goToPreviousQuestionCompletions = []
-//        goToNextQuestionCompletions = []
-//        answerCompletions = []
-//    }
-//}
-
 class DelegateSpy: QuizDelegate {
     var questionsAsked: [String] = []
     var goToPreviousQuestionCompletions: [(() -> Void)?] = []
